daeca30d25341803459b0929ec6e17768bdda6c1
Reza <r.ghorbani.f@gmail.com>
2018-01-29 14:30:55 +0330

    fb

--- src/commons/BaseComponent.js
+++ src/commons/BaseComponent.js
@@ -5,6 +5,7 @@
  */
 
 const React = require('react');
+const _ = require('lodash');
 const { StyleSheet } = require('react-native');
 
 class BaseComponent extends React.Component {
@@ -16,6 +17,20 @@ class BaseComponent extends React.Component {
     if (!this.styles) {
       this.generateStyles();
     }
+  }
+
+  extractTypographyValue() {
+    const typographyPropsKeys = _.chain(this.props)
+      .keys(this.props)
+      .filter(key => Typography.getKeysPattern().test(key))
+      .value();
+    let typography;
+    _.forEach(typographyPropsKeys, (key) => {
+      if (this.props[key] === true) {
+        typography = Typography[key];
+      }
+    });
+    return typography;
   }
 
   generateStyles() {
